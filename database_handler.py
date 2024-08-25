2ed8ae2d725b66e6dffa758e5a2383e106540d5e
Laurits Madsen <laurits.skov.madsen@gmail.com>
2024-08-25 11:04:33 +0200

    Added update_content and update_content_by_title functions

--- database_handler.py
+++ database_handler.py
@@ -11,7 +11,24 @@ def add_project(title: str, content_path: str, description: str, main_image_path
     cursor.execute('INSERT INTO projects (title, content, description, main_image, date) VALUES (?, ?, ?, ?, ?)', (title, content, description, main_image_path, current_date))
     connect.commit()
 
-add_project("This is another test",
-            'projects/example.md',
-            'Here is a test being described in the most descriptive way possible. No one can describe it better in any way.',
-            'https://cdn.britannica.com/92/212692-050-D53981F5/labradoodle-dog-stick-running-grass.jpg')
+def update_content(id: str, content_path: str):
+    current_date = date.today().strftime('%d-%m-%Y')
+    with open(content_path, 'r', encoding='utf-8') as read_file:
+        content = read_file.read()
+
+    connect = sqlite3.connect('database.db')
+    cursor = connect.cursor()
+    cursor.execute("UPDATE projects SET content = ?, date = ? WHERE id = ?", (content, current_date, id))
+    connect.commit()
+
+def update_content_by_title(title: str, content_path: str):
+    connect = sqlite3.connect('database.db')
+    cursor = connect.cursor()
+    cursor.execute("SELECT id FROM projects WHERE title = ?", (title,))
+    id = cursor.fetchone()
+    cursor.close()
+    connect.close()
+    update_content(id[0], content_path)
+
+
+update_content_by_title("This is a title", "projects/example.md")
