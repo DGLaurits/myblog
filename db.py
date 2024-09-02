a0fedc9bd28e64698e30316d5fad38ae7951aaab
Laurits Madsen <laurits.skov.madsen@gmail.com>
2024-09-02 09:36:51 +0200

    Add delete functionality to database and reset projects

--- db.py
+++ db.py
@@ -9,24 +9,27 @@ def add_project(title: str, content: str, description: str, main_image_path: str
     cursor.execute('INSERT INTO projects (title, content, description, main_image, date) VALUES (?, ?, ?, ?, ?)', (title, content, description, main_image_path, current_date))
     connect.commit()
 
-def update_content(id: str, content_path: str):
+def update_content(id: str, content: str):
     current_date = date.today().strftime('%d-%m-%Y')
-    with open(content_path, 'r', encoding='utf-8') as read_file:
-        content = read_file.read()
-
     connect = sqlite3.connect('database.db')
     cursor = connect.cursor()
     cursor.execute("UPDATE projects SET content = ?, date = ? WHERE id = ?", (content, current_date, id))
     connect.commit()
 
-def update_content_by_title(title: str, content_path: str):
+def update_content_by_title(title: str, content: str):
     connect = sqlite3.connect('database.db')
     cursor = connect.cursor()
     cursor.execute("SELECT id FROM projects WHERE title = ?", (title,))
     id = cursor.fetchone()
     cursor.close()
     connect.close()
-    update_content(id[0], content_path)
+    update_content(id[0], content)
+
+def delete_project(id: int):
+    connect = sqlite3.connect('database.db')
+    cursor = connect.cursor()
+    cursor.execute("DELETE FROM projects WHERE id = ?", (id,))
+    connect.commit()
 
 def get_projects(amount: int):
     db = sqlite3.connect("database.db")
